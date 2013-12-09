dc67878df0d3a4756ff2c1ad1483451b663d4ba2
ericnordlund <ericnordlund@ip113-182.wireless.lu.se>
2013-12-09 13:57:40 +0100

    La till lite rolig text.

--- src/db2/dev/DB2Dev.java
+++ src/db2/dev/DB2Dev.java
@@ -16,7 +16,7 @@ public class DB2Dev {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("DÖÖÖ");
     }
     
 }
