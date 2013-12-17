bd4a5370d24afaf0d16ea4d5fbd3f6ff0dc64f83
Eric <Eric@192.168.0.10>
2013-12-17 14:24:51 +0100

    Flyttade över samtliga studentfunktioner till DALen.

--- DBDEV/src/Model/Dal.java
+++ DBDEV/src/Model/Dal.java
@@ -11,6 +11,12 @@ public class Dal {
     private Statement statement;
     
     
+    /*
+     * ********************************************************************************************
+     * ********************************DATABASE CONNECTION METHODS********************************
+     * ********************************************************************************************
+     */
+    
     /**
      * Konstruktor: Initierar en anslutning mot servern samt laddar drivrutinerna och skapar en statement.
      * @throws SQLException 
@@ -83,5 +89,75 @@ public class Dal {
         this.con.close();
         
     }
+    
+        /*
+     * ********************************************************************************************
+     * ********************************STUDENT QUERYS**********************************************
+     * ********************************************************************************************
+     */
+    
+    /**
+     * Ger ett resultset med alla studenter i.
+     * @return 
+     * @throws java.sql.SQLException 
+     */
+    public ResultSet getAllStudents() throws SQLException
+    {
+        String query = "SELECT * FROM student";
+        ResultSet result = getQuery(query);
+        System.out.println("Getting student list.");
+        return result;  
+    }
+     
+    
+    /**
+     * En funktion som hämtar samtlig data för en student.
+     * @param studentID ID på den student som är av intresse.
+     * @return Samtliga data i form av ett resultset.
+     * @throws SQLException 
+     */
+    public ResultSet getSingleStudent(int studentID) throws SQLException {
+        String query = "SELECT * FROM student WHERE" + studentID;
+        ResultSet result = getQuery(query);
+        return result;
+        
+    }
+    
+    /**
+     * Uppdaterar uppgifterna för en student
+     * @param studentID ID på studenten som skall uppdateras.
+     * @param name Det nya eller förändrade namnet.
+     * @param adress Den nya eller förändrade adressen. 
+     * @throws SQLException 
+     */
+    public void modifyStudent(int studentID, String name, String adress) throws SQLException {
+        String query = "UPDATE student SET name='" + name + "', adress='" + adress + "' WHERE studentID='" + studentID + "'";
+        sendQuery(query);
+        System.out.println("Updated student " + studentID + ".");
+    }
+    
+    /**
+     * Skapar en student i tabellen Student
+     * @param name Namnet på den nye studenten.
+     * @param adress Adressen på den nye studenten.
+     * @throws SQLException 
+     */
+    public void insertStudent(String name, String adress) throws SQLException {
+        String query = "INSERT INTO student (name, adress) VALUES('" + name + "', '" + adress + "')";
+        sendQuery(query);
+        System.out.println("Registered new student.");
+    }
+    
+    /**
+     * Tar bort en student från databasen.
+     * @param studentID ID på den student som skall tas bort. 
+     * @throws SQLException 
+     */
+    public void removeStudent(int studentID) throws SQLException {
+        String query = "DELETE FROM student WHERE studentID = " + studentID;
+        sendQuery(query);
+        System.out.println("Removed student " + studentID + ".");
+    }
+    
     
 }
